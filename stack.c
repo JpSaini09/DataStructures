0c396e43430c7476727a90509a96874fdb8f7cc9
JpSaini09 <jaspreetsaini1996@gmail.com>
2016-08-16 18:55:18 +0530

    done

---
+++ stack.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<malloc.h>
+#include"stack.h"
+struct node
+{
+    void* data;
+    struct node* next; 
+};
+static struct node* head = NULL;
+static int n = 0;
+void push(void* value)
+{
+    struct node* add = head;
+    head = (struct node*)malloc(sizeof(struct node));
+    head->data = value;
+    head->next = add;
+    n++;
+}
+void* pop()
+{
+    void* val = head->(void*)data;
+    head = head->next;
+    n--;
+    return val;
+}
+int isempty()
+{i
+    return head == NULL;
+}
+int size()
+{
+    return n;
+}
